4771c1028a99048a159b9c72c82f31b1ebe31e54
sittisak <sittisak@proteus-tech.com>
2017-04-01 22:35:13 +0700

    add UID

--- mix.sh
+++ mix.sh
@@ -1,2 +1,3 @@
+export UID
 docker-compose run --rm roammeet-people-service sh -c "mix local.hex --force && mix local.rebar --force"
 docker-compose run --rm roammeet-meetup-service sh -c "mix local.hex --force && mix local.rebar --force"
