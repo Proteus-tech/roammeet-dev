198c891d722de757fac3872aba5b610fca44c6d6
sakgunner <sakgunner@gmail.com>
2017-04-01 23:47:13 +0700

    fixed migrate for mac

--- migrate-mac.sh
+++ migrate-mac.sh
@@ -1,2 +1,3 @@
+export UID
 docker-compose -f docker-compose-mac.yml run --rm roammeet-people-service sh -c "cd /usr/src/people && mix ecto.migrate"
 docker-compose -f docker-compose-mac.yml run --rm roammeet-meetup-service sh -c "cd /usr/src/meetup && mix ecto.migrate"
